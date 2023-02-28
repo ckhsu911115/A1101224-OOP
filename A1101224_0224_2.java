5f497eb1fb3e3bb70e4e1919d9871ba028abe2e8
ckhsu911115 <58980251+ckhsu911115@users.noreply.github.com>
2023-02-28 17:59:43 +0800

    A1101224_0224_2.java

---
+++ A1101224_0224_2.java
@@ -0,0 +1,18 @@
+import java.util .*;
+
+public class Q10 {
+    public static void main(String[] argv) {
+        int num;
+        System.out.println("請輸入攝氏　？　℃(僅能輸入數字):");
+        Scanner sc = new Scanner(System.in);
+
+        num = ((sc.nextInt())-32)*5/9;
+        System.out.print("換算為"+ num + "°F");
+
+
+        }
+
+
+     }
+
+
